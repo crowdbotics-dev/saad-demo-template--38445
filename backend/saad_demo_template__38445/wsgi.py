fe9e634a01bee7564a4584ac742bb92b2a842faf
Crowdbotics <engineering@crowdbotics.com>
2022-10-13 09:10:56 +0000

    Update models

--- backend/saad_demo_template__38445/wsgi.py
+++ backend/saad_demo_template__38445/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saad_demo_template__38445.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saad_demo_template__38445.settings")
 
 application = get_wsgi_application()
